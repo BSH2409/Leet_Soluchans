6c1d50840b5548c5607489810a0ba0beac0917be
Bhartik Harchand <bsh.bhartik@gmail.com>
2022-08-31 16:25:13 +0530

    Time: 4 ms (35.94%), Space: 7.8 MB (25.51%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    unordered_map<int,int> dp;
-    int help(int i, vector<int>&nums)
-    {
-        if(i<0) return 0;
-        if(dp.find(i)!=dp.end()) return dp[i];
-        int leave = 0+ help(i-1,nums);
-        int take = nums[i] + help(i-2,nums);
+//     unordered_map<int,int> dp;
+//     int help(int i, vector<int>&nums)
+//     {
+//         if(i<0) return 0;
+//         if(dp.find(i)!=dp.end()) return dp[i];
+//         int leave = 0+ help(i-1,nums);
+//         int take = nums[i] + help(i-2,nums);
         
-        return dp[i]=max(leave,take);
-    }
+//         return dp[i]=max(leave,take);
+//     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        return help(n-1,nums);
+        if(n==1) return nums[0];
+        vector<int> dp(n+1,0);
+        dp[0]=nums[0];
+        dp[1] = max(nums[1],nums[0]);
+        for(int i=2;i<n;i++)
+        {
+            int leave= 0+ dp[i-1];
+            int take= nums[i]+dp[i-2];
+            dp[i]=max(leave,take);
+        }
+        
+        return dp[n-1];
+
     }
 };
