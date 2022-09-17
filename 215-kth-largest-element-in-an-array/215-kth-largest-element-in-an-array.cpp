fa9b276d752b84118d946bb1224257d872cacd00
Bhartik Harchand <bsh.bhartik@gmail.com>
2022-09-17 23:31:59 +0530

    Time: 1289 ms (26.26%), Space: 27.3 MB (45.81%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,9 +1,9 @@
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue <int> n(nums.begin(),nums.end());
-        for(int i=1;i<k;i++)
-            n.pop();
-        return n.top();
-    }
-};
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        pq = []
+        for i in nums:
+            heapq.heappush(pq, i)
+            if len(pq) > k:
+                heapq.heappop(pq)
+        return heapq.heappop(pq)
