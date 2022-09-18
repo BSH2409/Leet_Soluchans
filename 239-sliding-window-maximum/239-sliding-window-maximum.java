9c7f4052d8abf747d8ee004e8daa1df512e0aa6e
Bhartik Harchand <bsh.bhartik@gmail.com>
2022-09-18 15:32:24 +0530

    Time: 72 ms (40.52%), Space: 150.6 MB (47.23%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,18 +1,27 @@
 
     class Solution {
-    public int[] maxSlidingWindow(int[] arr, int k) {
-        int n = arr.length, j = 0;
-        int[] ans = new int[n - k + 1];
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        for (int i = 0; i < n; i++) {
-            maxHeap.add(new int[]{arr[i], i});
-            if (i + 1 >= k)
-                
-                ans[j++] = maxHeap.peek()[0];
-            
-            while (maxHeap.size() > 0 && i - maxHeap.peek()[1] + 1 >= k) 
-                maxHeap.poll();
-        }
-        return ans;
+    public int[] maxSlidingWindow(int[] nums, int k) {
+  int n = nums.length;
+  
+  int[] result = new int[n - k + 1];
+  Deque<Integer> win = new ArrayDeque<>(); 
+  
+  for (int i = 0; i < n; ++i) {
+      
+    while (win.size() > 0 && win.peekFirst() <= i - k) {
+      win.pollFirst();
     }
+      
+    while (win.size() > 0 && nums[win.peekLast()] < nums[i]) {
+      win.pollLast();
+    }
+      
+    win.offerLast(i);
+      
+    if (i >= k - 1) {
+      result[i - k + 1] = nums[win.peekFirst()];
+    }
+  }
+  return result;
+}
 }
