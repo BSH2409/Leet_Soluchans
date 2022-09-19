00824e34926961caa3a7a717e21b4f711f81b5ae
Bhartik Harchand <bsh.bhartik@gmail.com>
2022-09-19 05:52:44 +0530

    Time: 3258 ms (29.83%), Space: 14 MB (83.22%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.py
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.py
@@ -1,48 +1,41 @@
-class Solution {
-    public List<String> removeInvalidParentheses(String s) {
-     int del_left = 0, del_right = 0;
-     for (int i = 0; i < s.length(); i++) {
-         if (s.charAt(i) == '(') {
-             del_left++;
-         } else if (s.charAt(i) == ')') {
-             if (del_left != 0) {
-                 del_left--;
-             } else {
-                 del_right++;
-             }
-         }
-     }
-     Set<String> res = new HashSet<>();
-     dfs(s, 0, res, new StringBuilder(), del_left, del_right, 0);
-     return new ArrayList<String>(res);
- }
- 
- public void dfs(String s, int i, Set<String> res, StringBuilder seq, int del_left, int del_right, int open) {
-     if (del_left < 0 || del_right < 0 || open < 0) {
-         return;
-     }
-     if (i == s.length()) {
-         if (del_left == 0 && del_right == 0 && open == 0) {
-             res.add(seq.toString());
-         }        
-         return;
-     }
- 
-     char c = s.charAt(i); 
-     int len = seq.length();
- 
-     if (c == '(') {
-         dfs(s, i + 1, res, seq, del_left - 1, del_right, open);		    
-         dfs(s, i + 1, res, seq.append(c), del_left, del_right, open + 1);      
- 
-     } else if (c == ')') {
-         dfs(s, i + 1, res, seq, del_left, del_right - 1, open);	            
-         dfs(s, i + 1, res, seq.append(c), del_left, del_right, open - 1);  	   
- 
-     } else {
-         dfs(s, i + 1, res, seq.append(c), del_left, del_right, open);	
-     }
- 
-     seq.setLength(len);        
- }
- }
+class Solution:
+    def removeInvalidParentheses(self,s):
+
+        del_left=0
+        del_right=0
+        
+        for i in s:
+            if i=="(":
+                del_left+=1
+            elif i==")":
+                if del_left>0:
+                    del_left-=1
+                else:
+                    del_right+=1
+        
+        ans=set()
+        
+        def dfs(index,left,right,del_left,del_right,seq):
+            if index==len(s):
+                if left==right and del_left==0 and del_right==0:
+                    ans.add(seq)
+                return 
+            
+            if s[index]=='(':
+                if del_left>0:
+                    dfs(index+1,left,right,del_left-1,del_right,seq)
+                    
+                dfs(index+1,left+1,right,del_left,del_right,seq+"(")
+                
+            elif s[index]==')':
+                if del_right>0:
+                    dfs(index+1,left,right,del_left,del_right-1,seq)
+                if  right<left:  
+                    dfs(index+1,left,right+1,del_left,del_right,seq+")")
+            else:
+                dfs(index+1,left,right,del_left,del_right,seq+s[index])
+                
+                    
+                
+        dfs(0,0,0,del_left,del_right,"")
+        return list(ans)
