4a4f87575d34234a805629196a5bfc84a83ee85c
Bhartik Harchand <bsh.bhartik@gmail.com>
2022-09-25 15:46:26 +0530

    Time: 230 ms (39.57%), Space: 14.7 MB (44.35%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,9 +1,5 @@
-class Solution {
-   public int missingNumber(int[] nums) { //sum
-    int n = nums.length;
-    int sum = n*(n+1)/2;
-    for(int i=0; i<n; i++)
-        sum-=nums[i];
-    return sum;
-}
-}
+class Solution(object):
+    def missingNumber(self, nums):
+        n= len(nums)
+        return n*(n+1)/2 - sum(nums)
+        
