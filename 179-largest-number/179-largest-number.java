086d303f05d2a9cca9166aab54786140fbb48335
Bhartik Harchand <bsh.bhartik@gmail.com>
2022-09-25 23:47:49 +0530

    Time: 15 ms (20.06%), Space: 44.2 MB (30.23%) - LeetHub

--- 179-largest-number/179-largest-number.java
+++ 179-largest-number/179-largest-number.java
@@ -1,22 +1,24 @@
 class Solution {
-    public String largestNumber(int[] nums) {
-       if (nums == null || nums.length == 0) {
-           return "";
-       }
-       String[] s_num = new String[nums.length];
-       for (int i = 0; i < nums.length; i++) {
-           s_num[i] = String.valueOf(nums[i]);
-       }
-       Arrays.sort(s_num, (a, b) -> (b + a).compareTo(a + b));
-       
-       if (s_num[0].equals("0")) {
-            return "0";
-       }
-       StringBuilder sb = new StringBuilder();
-       for (String str : s_num) {
-           sb.append(str);
-       }
-       return sb.toString();
-           
+    
+
+public String largestNumber(int[] nums) {
+    if (nums == null || nums.length == 0) {
+        return "";
     }
+    String[] largest = new String[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+        largest[i] = String.valueOf(nums[i]);
+    }
+    Arrays.sort(largest, (a, b) -> (b + a).compareTo(a + b));
+    
+    if (largest[0].equals("0")) {
+         return "0";
+    }
+    StringBuilder res = new StringBuilder();
+    for (String str : largest) {
+        res.append(str);
+    }
+    return res.toString();
+        
+ }
 }
