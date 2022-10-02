5253d167db77fb14b840c8f1c7a470dfafb2c4d1
Bhartik Harchand <bsh.bhartik@gmail.com>
2022-10-02 07:46:25 +0530

    Time: 128 ms (20.62%), Space: 69.8 MB (61.15%) - LeetHub

--- 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.java
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.java
@@ -1,20 +1,21 @@
 class Solution {
-     public int subarraysWithKDistinct(int[] nums, int k) {
+     
+    public int subarraysWithKDistinct(int[] nums, int k) {
         return subarraysWithKSize(nums,k)-subarraysWithKSize(nums,k-1);   
        }
     public int subarraysWithKSize(int[] nums, int k) {
-            HashMap<Integer,Integer> hs=new HashMap<>();
+            HashMap<Integer,Integer> hash=new HashMap<>();
             int count=0;
             int n=nums.length;
             int window_start=0;
             int window_end=0;
             while(window_end<n){
-                hs.put(nums[window_end],hs.getOrDefault(nums[window_end],0)+1);
+                hash.put(nums[window_end],hash.getOrDefault(nums[window_end],0)+1);
             
-                while(hs.size()==k+1){
-                    hs.put(nums[window_start],hs.get(nums[window_start])-1);
-                    if(hs.get(nums[window_start])==0)
-                        hs.remove(nums[window_start]);
+                while(hash.size()==k+1){
+                    hash.put(nums[window_start],hash.get(nums[window_start])-1);
+                    if(hash.get(nums[window_start])==0)
+                        hash.remove(nums[window_start]);
                     window_start++;
                     
                 }
