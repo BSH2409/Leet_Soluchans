d3e723d3d33dff67208c2cee421d12036c750e39
Bhartik Harchand <bsh.bhartik@gmail.com>
2022-10-13 16:18:48 +0530

    Time: 643 ms (64.24%), Space: 14.2 MB (94.16%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,9 @@
-class Solution {
-    public boolean canJump(int[] nums) {
-        int max_reach = 0; 
-    
-        for(int i= 0; i< nums.length; i++) {
-            if(max_reach < i) return false;
-            max_reach = Math.max(max_reach, i+ nums[i]);
-        }
-        return true;
-    }
-}
+class Solution(object):
+    def canJump(self, nums):
+        max_reach=0
+        for i in range(len(nums)):
+            if max_reach < i:
+                return False
+            max_reach = max(max_reach,i+nums[i])
+        return True
+        
