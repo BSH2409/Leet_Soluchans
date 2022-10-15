7eea60a6127d78412e9e93f03b969095adc60708
Bhartik Harchand <bsh.bhartik@gmail.com>
2022-10-15 16:53:07 +0530

    Time: 55 ms (15.76%), Space: 51.1 MB (13.46%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,27 +1,27 @@
 class Solution {
+    Integer dp[][];
     public int countSubstrings(String s) {
         int ans=0;
         dp=new Integer[s.length()][s.length()];
         for(int i=0;i<s.length();i++) {
             for(int j=i;j<s.length();j++) {
-                int ret=util(s, i, j);
+                int ret=help(s, i, j);
                 ans+=ret;
             }
         }
         return ans;
     }
-    Integer dp[][];
-    private Integer util(String s, int i, int j) {
-        if(i>=j) {
+    
+    public Integer help(String s, int i, int j) {
+        if(i>=j) 
             return 1;
-        }
         
         if(dp[i][j]!=null) return dp[i][j];
         
         if(s.charAt(i)==s.charAt(j))
-            return util(s, i+1, j-1);
+            return dp[i][j]=help(s, i+1, j-1);
         
-        return 0;
+        return dp[i][j]=0;
             
-        }
+    }
 };
