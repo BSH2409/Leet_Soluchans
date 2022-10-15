891f21cd12f88cf9b9609096a677c9eb9e218c6c
Bhartik Harchand <bsh.bhartik@gmail.com>
2022-10-15 18:14:45 +0530

    Time: 1 ms (100.00%), Space: 42.1 MB (59.02%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,27 +1,30 @@
 class Solution {
-    Integer dp[][];
     public int countSubstrings(String s) {
-        int ans=0;
-        dp=new Integer[s.length()][s.length()];
-        for(int i=0;i<s.length();i++) {
-            for(int j=i;j<s.length();j++) {
-                int ret=help(s, i, j);
-                ans+=ret;
+        int len= s.length();
+        int result = 0;
+        int i = 0;
+        while (i < len) {
+            int start = i++;
+            while (i < len && s.charAt(start) == s.charAt(i)) {
+                i++;
             }
+			
+            int centerLen = i - start;
+            result += centerLen * (centerLen + 1) / 2;
+
+            result += expandPalindrome(s, start - 1, i);
         }
-        return ans;
+
+        return result;
     }
-    
-    public Integer help(String s, int i, int j) {
-        if(i>=j) 
-            return 1;
-        
-        if(dp[i][j]!=null) return dp[i][j];
-        
-        if(s.charAt(i)==s.charAt(j))
-            return dp[i][j]=help(s, i+1, j-1);
-        
-        return dp[i][j]=0;
-            
+
+    private int expandPalindrome(String s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+            count++;
+        }
+        return count;
     }
-};
+}
