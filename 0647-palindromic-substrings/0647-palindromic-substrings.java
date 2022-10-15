d9089c1bbec90b585aa4885ae4307178440c9651
Bhartik Harchand <bsh.bhartik@gmail.com>
2022-10-15 18:16:44 +0530

    Time: 2 ms (99.16%), Space: 42 MB (70.77%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,24 +1,23 @@
 class Solution {
     public int countSubstrings(String s) {
-        int len= s.length();
-        int result = 0;
+        int n= s.length();
+        int res= 0;
         int i = 0;
-        while (i < len) {
+        while (i < n) {
             int start = i++;
-            while (i < len && s.charAt(start) == s.charAt(i)) {
+            while (i < n && s.charAt(start) == s.charAt(i)) 
                 i++;
-            }
 			
-            int centerLen = i - start;
-            result += centerLen * (centerLen + 1) / 2;
+            int repeats = i - start;
+            res += repeats * (repeats + 1) / 2;
 
-            result += expandPalindrome(s, start - 1, i);
+            res += help(s, start - 1, i);
         }
 
-        return result;
+        return res;
     }
 
-    private int expandPalindrome(String s, int left, int right) {
+    private int help(String s, int left, int right) {
         int count = 0;
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
